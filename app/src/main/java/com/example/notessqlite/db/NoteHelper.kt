6ef322447068e5122251f13faa9bfb9a66d2c154
satriomuslim <satriomuslim2627@gmail.com>
2023-06-17 09:58:57 +0700

    menu dll

--- app/src/main/java/com/example/notessqlite/db/NoteHelper.kt
+++ app/src/main/java/com/example/notessqlite/db/NoteHelper.kt
@@ -1,8 +1,11 @@
 package com.example.notessqlite.db
 
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import com.example.notessqlite.db.DatabaseContract.NoteColumns.Companion.TABLE_NAME
+import com.example.notessqlite.db.DatabaseContract.NoteColumns.Companion._ID
 import java.sql.SQLException
 
 class NoteHelper(context: Context) {
@@ -32,6 +35,44 @@ class NoteHelper(context: Context) {
 
         if (database.isOpen)
             database.close()
+    }
+
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$_ID ASC",
+            null
+        )
+    }
+
+    fun queryById(id: String): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            "$_ID = ?",
+            arrayOf(id),
+            null,
+            null,
+            null,
+            null,
+        )
+    }
+
+    fun insert(values: ContentValues?): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+
+    fun update(id: String, values: ContentValues?): Int {
+        return database.update(DATABASE_TABLE, values, "$_ID = ?", arrayOf(id))
+    }
+
+    fun deleteById(id: String): Int {
+        return database.delete(DATABASE_TABLE, "$_ID = '$id'", null)
     }
 
 }
